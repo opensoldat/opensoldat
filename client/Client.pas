67b2837c1b61f015fe31a097e524512c898eb7f8
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2020-10-01 00:33:50 +0200

    Unpack default taunts on first launch

--- client/Client.pas
+++ client/Client.pas
@@ -710,6 +710,7 @@ begin
   LoadInterfaceArchives(UserDirectory + 'custom-interfaces/');
 
   PHYSFS_CopyFileFromArchive('configs/client.cfg', UserDirectory + '/configs/client.cfg');
+  PHYSFS_CopyFileFromArchive('configs/taunts.cfg', UserDirectory + '/configs/taunts.cfg');
 
   LoadConfig('client.cfg');
 
