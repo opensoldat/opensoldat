2d74bc269755cf1f41ef551f6fa7b60b9af988bb
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2020-10-04 00:50:19 +0200

    Simpler approach for downloading assets with CMake

--- cmake_modules/SoldatAssets.cmake
+++ cmake_modules/SoldatAssets.cmake
@@ -9,38 +9,34 @@ macro(download_assets)
       CACHE STRING "We pull soldat.smod and font from here")
   set(BASE_GIT_TAG "v0.1"
       CACHE STRING "Git tag associated with release in base repository")
-
+  set(SOLDAT_SMOD_SHA1 "491be267e4279b4a1634f2cff4a84efc69bef83c")
   set(DOWNLOAD_URL ${BASE_REPOSITORY_URL}/releases/download/${BASE_GIT_TAG})
+  set(DOWNLOADS_DIR downloads)
+
   message(STATUS "Soldat assets will be downloaded from ${DOWNLOAD_URL}")
-  FetchContent_Declare(
-    smod
-    URL ${DOWNLOAD_URL}/soldat.smod
-    DOWNLOAD_NO_EXTRACT TRUE # By default, FetchContent unzips the file. We don't want that.
-  )
-  FetchContent_Declare(
-    font
-    URL ${DOWNLOAD_URL}/play-regular.ttf
-    DOWNLOAD_NO_EXTRACT TRUE # Once again, we don't want any unzipping.
+  file(DOWNLOAD
+    ${DOWNLOAD_URL}/soldat.smod
+    ${DOWNLOADS_DIR}/soldat.smod
+    EXPECTED_HASH SHA1=${SOLDAT_SMOD_SHA1}
   )
 
-  message(STATUS "Retrieving soldat.smod")
-  FetchContent_MakeAvailable(smod)
-
-  message(STATUS "Retrieving soldat font")
-  FetchContent_MakeAvailable(font)
+  file(DOWNLOAD
+    ${DOWNLOAD_URL}/play-regular.ttf
+    ${DOWNLOADS_DIR}/play-regular.ttf
+  )
 
   # Configure targets, so that we can set dependencies
   # on client and server. This will make sure that
   # asset files get copied during build step (make).
   add_custom_target(
     soldat_smod
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${smod_SOURCE_DIR}/soldat.smod
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${DOWNLOADS_DIR}/soldat.smod
                                                   ${EXECUTABLE_OUTPUT_PATH}/soldat.smod
     COMMENT "Copying soldat.smod to ${EXECUTABLE_OUTPUT_PATH}"
   )
   add_custom_target(
     soldat_font
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${font_SOURCE_DIR}/play-regular.ttf
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${DOWNLOADS_DIR}/play-regular.ttf
                                                   ${EXECUTABLE_OUTPUT_PATH}/play-regular.ttf
     COMMENT "Copying font to ${EXECUTABLE_OUTPUT_PATH}"
   )
