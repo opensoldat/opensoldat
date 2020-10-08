90c41625bca57b0947cf00be7a9e9d6c0b6ce99b
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2020-10-08 23:04:10 +0200

    Bugfix. Client crashes when joining game while being banned

--- shared/network/NetworkClientConnection.pas
+++ shared/network/NetworkClientConnection.pas
@@ -459,7 +459,7 @@ end;
 procedure ClientHandleUnAccepted(NetMessage: PSteamNetworkingMessage_t);
 var
   UnAcceptedMsg: PMsg_UnAccepted;
-  Text: WideString;
+  Text: String;
   TextLen: Integer;
 begin
   if not VerifyPacketLargerOrEqual(sizeof(UnAcceptedMsg), NetMessage^.m_cbSize, MsgID_UnAccepted) then
@@ -468,7 +468,7 @@ begin
   UnAcceptedMsg := PMsg_UnAccepted(NetMessage^.m_pData);
   TextLen := NetMessage^.m_cbSize - SizeOf(TMsg_UnAccepted);
 
-  if (TextLen > 0) and (UnAcceptedMsg.Text[TextLen-1] = #0) then
+  if (TextLen > 0) and (PChar(UnAcceptedMsg.Text)[TextLen-1] = #0) then
     Text := PChar(UnAcceptedMsg.Text)
   else
     Text := '';
