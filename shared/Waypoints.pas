f61856d6911ce1f3f7ad522a3a0633e8cc218b80
helloer <im.helloer@gmail.com>
2021-08-16 12:07:38 +0200

    Refactor Waypoints unit

--- shared/Waypoints.pas
+++ shared/Waypoints.pas
@@ -15,14 +15,15 @@ const
   MAX_CONNECTIONS = 20;
 
 type
+  {$scopedenums on}
+  TWaypointAction = (None, StopAndCamp, Wait1Second, Wait5Seconds, Wait10Seconds, Wait15Seconds, Wait20Seconds);
   TWaypoint = record
     Active: Boolean;
-    id: Integer;
+    Id: Integer;
     X, Y: Integer;
-    left, right, up, down, m2: Boolean;
-    // PathNum: Integer;
+    Left, Right, Up, Down, Jetpack: Boolean;
     PathNum: Byte;
-    C1, C2, C3: Byte;
+    Action: TWaypointAction;
     ConnectionsNum: Integer;
     Connections: array[1..MAX_CONNECTIONS] of Integer;
   end;
@@ -30,64 +31,13 @@ type
 type
   TWaypoints = object
     Waypoint: array[1..MAX_WAYPOINTS] of TWaypoint;
-
-    procedure LoadFromFile(filename: string);
-    procedure SaveToFile(filename: string);
     function FindClosest(X, Y: Single; Radius, CurrWaypoint: Integer): Integer;
-    function CreateWayPoint(sX, sY, sPath: Integer): Integer;
-  end;
-
-  // for file use
-  TPathRec = record
-    Waypoint: array[1..500] of TWaypoint;
   end;
 
 implementation
 
 uses
   Calc, SysUtils;
-
-procedure TWaypoints.LoadFromFile(filename: string);
-var
-  AddrFile: File of TPathRec;
-  AddrRec: TPathRec;
-  i, j: Integer;
-begin
-  AssignFile(AddrFile, Filename);
-  FileMode := fmOpenRead;
-  Reset(AddrFile);
-  Read(AddrFile, AddrRec);
-  CloseFile(AddrFile);
-
-  for i := 1 to MAX_WAYPOINTS do
-    Waypoint[i] := AddrRec.Waypoint[i];
-
-  for i := 1 to MAX_WAYPOINTS do
-    if not Waypoint[i].Active then
-    begin
-      Waypoint[i].ConnectionsNum := 0;
-      for j := 1 to MAX_CONNECTIONS do
-      begin
-        Waypoint[i].Connections[j] := 0;
-      end;
-    end;
-end;
-
-procedure TWaypoints.SaveToFile(filename: string);
-var
-  AddrFile: File of TPathRec;
-  AddrRec: TPathRec;
-  i: Integer;
-begin
-  for i := 1 to MAX_WAYPOINTS do
-    AddrRec.Waypoint[i] := Waypoint[i];
-
-  AssignFile(AddrFile, Filename);
-  FileMode := fmOpenReadWrite;
-  Rewrite(AddrFile);
-  Write(AddrFile, AddrRec);
-  CloseFile(AddrFile);
-end;
 
 function TWaypoints.FindClosest(X, Y: Single; Radius, CurrWaypoint: Integer):
   Integer;
@@ -107,33 +57,6 @@ begin
         Exit;
       end;
     end;
-end;
-
-function TWaypoints.CreateWayPoint(sX, sY, sPath: Integer): Integer;
-var
-  i: Integer;
-begin
-  for i := 1 to MAX_WAYPOINTS + 1 do
-  begin
-    if i = MAX_WAYPOINTS + 1 then
-    begin
-      result := -1;
-      Exit;
-    end;
-    if not Waypoint[i].Active then
-      break;
-  end;
-  // i is now the active waypoint
-
-  Waypoint[i].Active := true;
-  Waypoint[i].X := sX;
-  Waypoint[i].Y := sY;
-  Waypoint[i].id := i;
-  Waypoint[i].ConnectionsNum := 0;
-  Waypoint[i].PathNum := sPath;
-
-  // activate waypoint
-  Result := i;
 end;
 
 end.
