bb76b2d6f41adc232cbf21fc7f51d66f7d081ed1
Brendan Dougherty <brandougherty1@gmail.com>
2022-07-17 18:39:41 -0500

    Translate weapon name loading message.

--- client/Client.pas
+++ client/Client.pas
@@ -342,7 +342,7 @@ begin
   else 
     Prefix := '';
 
-  MainConsole.Console('Loading Weapon Names from ' + Prefix + 'txt/weaponnames.txt', DEBUG_MESSAGE_COLOR);
+  MainConsole.Console(_('Loading Weapon Names from') + ' ' + Prefix + 'txt/weaponnames.txt', DEBUG_MESSAGE_COLOR);
   TF := PHYSFS_openRead(PChar(Prefix + 'txt/weaponnames.txt'));
   if TF <> nil then
     for i := 0 to EXTENDED_WEAPONS - 1 do
