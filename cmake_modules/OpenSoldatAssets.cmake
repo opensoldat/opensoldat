223e8df28b4e51b65d3f374a6d8fea1d495f379c
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2022-07-26 16:29:39 +0200

    New version of soldat.smod

--- cmake_modules/OpenSoldatAssets.cmake
+++ cmake_modules/OpenSoldatAssets.cmake
@@ -5,11 +5,11 @@
 macro(download_assets)
   include(FetchContent)
 
-  set(BASE_REPOSITORY_URL "https://github.com/opensoldat/base"
+  set(BASE_REPOSITORY_URL "https://github.com/rzaba0/base"
       CACHE STRING "We pull soldat.smod and font from here")
-  set(BASE_GIT_TAG "v0.3"
+  set(BASE_GIT_TAG "v0.3.2"
       CACHE STRING "Git tag associated with release in base repository")
-  set(SOLDAT_SMOD_SHA1 "d1f14a8dcf567a4a460dfde736a5963fee57ece0"
+  set(SOLDAT_SMOD_SHA1 "6763f748d93726268e2cbe8fd2abee324be741f0"
       CACHE STRING "Expected SHA1 of soldat.smod download")
   set(DOWNLOAD_URL ${BASE_REPOSITORY_URL}/releases/download/${BASE_GIT_TAG})
   set(DOWNLOADS_DIR downloads)
