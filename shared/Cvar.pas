b31be47c3bce1bdaf603a4e3c3cec3a091948e6a
Brendan Dougherty <brandougherty1@gmail.com>
2022-08-27 10:41:18 -0500

    Enable logging to files by default.

--- shared/Cvar.pas
+++ shared/Cvar.pas
@@ -723,7 +723,7 @@ begin
   CvarsSync := TFPHashList.Create;
 
   log_level := TIntegerCvar.Add('log_level', 'Sets log level', LEVEL_OFF, [], nil, LEVEL_OFF, LEVEL_TRACE);
-  log_enable := TBooleanCvar.Add('log_enable', 'Enables logging to file', False, [], nil);
+  log_enable := TBooleanCvar.Add('log_enable', 'Enables logging to file', True, [], nil);
   log_filesupdate := TIntegerCvar.Add('log_filesupdate', 'How often the log files should be updated in ticks (60 ticks = 1 second)', 3600, [], nil, 0, MaxInt);
   {$IFDEF SERVER}
   log_timestamp := TBooleanCvar.Add('log_timestamp', 'Enables/Disables timestamps in console', False, [CVAR_SERVER], nil);
