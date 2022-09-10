b8b38d5c4431b6f564a29196a9d7ce8a4423fcc8
DeathByDenim <jarno@jarno.ca>
2022-09-10 11:24:45 -0400

    Where am I in GitHub Actions?

--- .github/workflows/macos-fix-libs.sh
+++ .github/workflows/macos-fix-libs.sh
@@ -9,9 +9,13 @@ getpngversion()
   version=$(otool -L ${1} | grep "libpng" | sed s/".*png[0-9]*\."// | sed s/"\.dylib.*"//)
 }
 
-echo "Starting from $(pwd)"
-
 cd build
+
+echo "Starting from $(pwd)"
+echo "This is here:"
+ls
+
+echo "Fixing libs"
 
 getversion "freetype" "opensoldat/opensoldat"
 freetypeversion=${version}
