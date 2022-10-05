d90339a0f9030cf4a7a282e73613e66f5b46abd7
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2022-10-05 13:09:03 +0200

    [Client] rework client to use new features of shared LauncherIPC class (JSON support, handle multiple commands from launcher, send identity message); send message when joining a server

--- client/ClientLauncherIPC.pas
+++ client/ClientLauncherIPC.pas
@@ -3,19 +3,44 @@ unit ClientLauncherIPC;
 interface
 
 uses
-  LauncherIPC;
+  LauncherIPC, fpjson;
 
 type
   TClientLauncherIPC = class(TLauncherIPC)
   protected
-    procedure HandleMessage(Message: String); override;
+    procedure HandleParsedMessage(MessageId: String; JSON: TJSONObject); override;
+    procedure SendIdentityMessage; override;
+  public
+    procedure SendJoinServerMessage(IP: String; Port: Integer);
   end;
 
 implementation
 
-procedure TClientLauncherIPC.HandleMessage(Message: String);
+uses
+  LauncherMessages;
+
+procedure TClientLauncherIPC.HandleParsedMessage(MessageId: String; JSON: TJSONObject);
 begin
-  HandleCommand(Message);
+  if MessageId = LauncherMessageIds.Commands then
+    HandleCommandsMessage(JSON);
+end;
+
+procedure TClientLauncherIPC.SendIdentityMessage;
+var
+  Message: TClientIdentityMessage;
+begin
+  Message := TClientIdentityMessage.Create;
+  SendObjectAsJSON(Message);
+  Message.Free;
+end;
+
+procedure TClientLauncherIPC.SendJoinServerMessage(IP: String; Port: Integer);
+var
+  Message: TJoinServerMessage;
+begin
+  Message := TJoinServerMessage.Create(IP, Port);
+  SendObjectAsJSON(Message);
+  Message.Free;
 end;
 
 end.
--- shared/LauncherMessages.pas
+++ shared/LauncherMessages.pas
@@ -8,18 +8,40 @@ uses
 type
   LauncherMessageIds = record
     const Commands = 'COMMANDS';
+    const ClientIdentity = 'CLIENT_ID';
     const ServerIdentity = 'SERVER_ID';
+    const JoinServer = 'JOIN_SERVER';
   end;
 
-  {$IFDEF SERVER}
-  TServerIdentityMessage = class(TPersistent)
-  private
+  TMessage = class(TPersistent)
+  protected
     FId: String;
-  public
-    constructor Create;
   published
     property id: String read FId write FId;
   end;
+
+  {$IFDEF SERVER}
+  TServerIdentityMessage = class(TMessage)
+  public
+    constructor Create;
+  end;
+  {$ELSE}
+  TClientIdentityMessage = class(TMessage)
+  public
+    constructor Create;
+  end;
+
+  TJoinServerMessage = class(TMessage)
+  private
+    FIP: String;
+    FPort: Integer;
+  public
+    constructor Create(IP: String; Port: Integer);
+  published
+    property ip: String read FIP write FIP;
+    property port: Integer read FPort write FPort;
+  end;
+
   {$ENDIF}
 
 implementation
@@ -28,6 +50,18 @@ implementation
 constructor TServerIdentityMessage.Create;
 begin
   FId := LauncherMessageIds.ServerIdentity;
+end;
+{$ELSE}
+constructor TClientIdentityMessage.Create;
+begin
+  FId := LauncherMessageIds.ClientIdentity;
+end;
+
+constructor TJoinServerMessage.Create(IP: String; Port: Integer);
+begin
+  FId := LauncherMessageIds.JoinServer;
+  FIP := IP;
+  FPort := Port;
 end;
 {$ENDIF}
 
