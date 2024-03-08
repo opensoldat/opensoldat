635995a7060bdd0dd7a1965efce0c4111bb805a1
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2024-03-08 06:35:57 +0100

    Modify indent assignments in version file

--- shared/Version.pas
+++ shared/Version.pas
@@ -12,15 +12,15 @@ unit Version;
 interface
 
 const
-  OPENSOLDAT_VERSION = {$INCLUDE Version.txt};
-  OPENSOLDAT_VERSION_CHARS = Length(OPENSOLDAT_VERSION);
-  OPENSOLDAT_VERSION_LONG = {$INCLUDE VersionLong.txt};
+  OPENSOLDAT_VERSION            = {$INCLUDE Version.txt};
+  OPENSOLDAT_VERSION_CHARS      = Length(OPENSOLDAT_VERSION);
+  OPENSOLDAT_VERSION_LONG       = {$INCLUDE VersionLong.txt};
   OPENSOLDAT_VERSION_LONG_CHARS = Length(OPENSOLDAT_VERSION_LONG);
   {$IFDEF SERVER}
-  DEDVERSION = '2.9.0';
-  COREVERSION = '2.5';
+  DEDVERSION   = '2.9.0';
+  COREVERSION  = '2.5';
   {$ELSE}
-  DEDVERSION = 'GAME';
+  DEDVERSION   = 'GAME';
   {$ENDIF}
   DEMO_VERSION = $0000;  // Version number for OpenSoldat demo files
 
